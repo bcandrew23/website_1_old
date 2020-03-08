ef2ceff41eb89fc733b5a8e9b8bc87e9283b3417
bcandrew23 <ajcollinstravel2@gmail.com>
2020-03-08 22:15:30 +0000

    new pages

--- index/script.js
+++ index/script.js
@@ -1,1 +1,13 @@
+var content = 'Before you insult A MAN TEst';
 
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+
+$(ele).hide().appendTo('p').each(function (i) {
+    $(this).delay(100 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
